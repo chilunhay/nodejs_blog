810ec65701b1f01429680db1e9ca885563cf9612
chilunhay <72005042+chilunhay@users.noreply.github.com>
2022-11-21 19:45:15 +0700

    Done part 2

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP Logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template Engine
 app.engine(
@@ -25,7 +32,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
+  console.log(req.query.q);
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+
+  res.send("");
 });
 
 app.listen(port, () => {
